e2f6156f669f576180f5b6e949ac6074f225116f
dskurbatov <dskurbatov13@gmail.com>
2014-10-20 13:53:54 -0700

    add specs.js file

---
+++ spec/specs.js
@@ -0,0 +1,11 @@
+describe("titleCase", function() {
+  it("capitalizes the first letter of a word", function() {
+    titleCase("cat").should.equal("Cat");
+  });
+
+  it("should capitalize all the words in a string", function(){
+  	titleCase("i love you").should.equal("I Love You");
+  });
+
+  it("should return true if a word ")  
+});
